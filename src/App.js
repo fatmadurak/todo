e03c218b9d4c9b55c8568f97876f4cc9cb46d980
fatmadurak <fatmabadillidurak@gmail.com>
2023-01-30 10:39:00 +0300

    c

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import './index.css';
 import Header from './components/Header';
-import Content from './components/Content';
+import Content from "./components/Content"
+
 
 function App() {
   return (
--- src/components/Content.js
+++ src/components/Content.js
@@ -2,36 +2,34 @@ import React from 'react'
 
 function Content() {
   return (
-    <section clasName="main">
-		<input clasName="toggle-all" type="checkbox" />
-		<label htmlFor="toggle-all">
-			Mark all as complete
-		</label>
+	<section className="main">
+	
 
-		<ul clasName="todo-list">
-			<li clasName="completed">
-				<div clasName="view">
-					<input clasName="toggle" type="checkbox" />
+		<ul className="todo-list">
+			<li className="completed">
+				<div className="view">
+					<input className="toggle" type="checkbox" />
 					<label>Learn JavaScript</label>
-					<button clasName="destroy"></button>
+					<button className="destroy"></button>
 				</div>
 			</li>
 			<li>
-				<div clasName="view">
-					<input clasName="toggle" type="checkbox" />
+				<div className="view">
+					<input className="toggle" type="checkbox" />
 					<label>Learn React</label>
-					<button clasName="destroy"></button>
+					<button className="destroy"></button>
 				</div>
 			</li>
 			<li>
-				<div clasName="view">
-					<input clasName="toggle" type="checkbox" />
+				<div className="view">
+					<input className="toggle" type="checkbox" />
 					<label>Have a life!</label>
-					<button clasName="destroy"></button>
+					<button className="destroy"></button>
 				</div>
 			</li>
 		</ul>
 	</section>
+
   )
 }
 
